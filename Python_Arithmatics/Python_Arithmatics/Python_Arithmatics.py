8a3b0e293b9eee0cc8fc27759ae0477b2e1b1a94
jrajan14 <j.rajan14@yahoo.com>
2023-08-02 16:05:47 +0530

    Initial Commit
    Added Python Basic Arithmetic code

---
+++ Python_Arithmatics/Python_Arithmatics/Python_Arithmatics.py
@@ -0,0 +1,31 @@
+# Python Basic Programs
+# Arithmatics in Python
+
+a = 20
+print ("Value of A = ", a) #Note that a comma is used to concat the Mentioned string and value of variable
+
+b = 10
+print ("Value of B = ", b)
+
+c = a + b               # Addition
+print ("A + B = ", c)
+
+d = a - b               # Subtraction
+print ("A - B = ", d)
+
+e = a * b               # Multiplication
+print ("A x B = ", e)
+
+f = a / b               # Division
+print ("A / B = ", f)
+
+###########################################
+
+g = a + 10             # Addition of variable and numeric value
+print ("G = ",g)
+
+h = "a" + "b"          # Concat two strings
+print ("H = ",h)
+
+i = str(a) + str(b)    # Concat values of a and b
+print ("I = ",i)
