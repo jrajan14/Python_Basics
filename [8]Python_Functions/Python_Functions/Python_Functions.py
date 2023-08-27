fdd50e6985faa2a3a288117811ad741e72b412dc
jrajan14 <j.rajan14@yahoo.com>
2023-08-28 00:05:50 +0530

    Added more examples

--- [8]Python_Functions/Python_Functions/Python_Functions.py
+++ [8]Python_Functions/Python_Functions/Python_Functions.py
@@ -61,3 +61,64 @@ print(greet("Bob"))  # Output: HELLO, BOB!
 # Method function
 text = "Hello, world!"
 print(text.upper())  # Output: HELLO, WORLD!
+
+## Parameter Passing
+
+# Positional Parameters
+def add(x, y):
+    return x + y
+
+result = add(5, 3)
+print(result)  # Output: 8
+
+# Keyword Parameters
+def greet(name, message):
+    return f"Hello, {name}! {message}"
+
+result = greet(message="How are you?", name="Alice")
+print(result)  # Output: Hello, Alice! How are you?
+
+# Default Parameters
+def power(base, exponent=2):
+    return base ** exponent
+
+print(power(3))      # Output: 9 (3^2)
+print(power(2, 4))   # Output: 16 (2^4)
+
+# Arbitrary Number of Parameters:
+def add_numbers(*args):
+    total = 0
+    for num in args:
+        total += num
+    return total
+
+print(add_numbers(1, 2, 3, 4))  # Output: 10
+
+def print_info(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+
+print_info(name="Alice", age=30, city="Wonderland")
+# Output:
+# name: Alice
+# age: 30
+# city: Wonderland
+
+
+# Unpacking Arguments
+def multiply(x, y):
+    return x * y
+
+nums = (2, 3)
+result = multiply(*nums)
+print(result)  # Output: 6
+
+# Passing Mutable Objects
+def modify_list(lst):
+    lst.append(4)
+
+numbers = [1, 2, 3]
+modify_list(numbers)
+print(numbers)  
+# Output: [1, 2, 3, 4]
+
